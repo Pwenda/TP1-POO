0df7fbd6a122efb610670d736fb7062bb66cb7d5
Emma <emma.deoliveira@ynov.com>
2026-01-08 17:48:57 +0100

    polymorphisme

--- src/com/emmad/tp/Main.java
+++ src/com/emmad/tp/Main.java
@@ -1,10 +1,26 @@
 package com.emmad.tp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("Norbert", 5, "Woof", "black");
-        Cow cow = new Cow("Norbert", 5, "Woof", "black");
-        dog.play();
-        cow.play();
+        Pet pet1 = new Dog("Norbert", 5, "Woof", "black");
+        Pet pet2 = new Cow("Patricia", 3, "Moo", "white");
+
+        pet1.play();
+        pet2.play();
+
+        List<animal> animals = new ArrayList<>();
+        animals.add((animal) pet1);
+        animals.add((animal) pet2);
+
+        for (animal a : animals) {
+            a.makeSound();
+
+            if (a instanceof Pet) {
+                ((Pet) a).play();
+            }
+        }
     }
 }
